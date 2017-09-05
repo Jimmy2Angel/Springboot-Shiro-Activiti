f8f0db2b0dfb7734001f8568ee0308b319b1189f
lollipop <a1334416010@gmail.com>
2017-09-05 22:17:17 +0800

    -u

---
+++ src/main/java/indi/baojie/supervision/controller/LoginController.java
@@ -0,0 +1,40 @@
+package indi.baojie.supervision.controller;
+
+import indi.baojie.supervision.domain.User;
+import org.activiti.engine.repository.Model;
+import org.apache.shiro.SecurityUtils;
+import org.apache.shiro.authc.AuthenticationException;
+import org.apache.shiro.authc.LockedAccountException;
+import org.apache.shiro.authc.UsernamePasswordToken;
+import org.apache.shiro.subject.Subject;
+import org.springframework.util.StringUtils;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import javax.servlet.http.HttpServletRequest;
+
+public class LoginController {
+
+    @PostMapping(value="/login")
+    public String login(HttpServletRequest request, User user, Model model){
+        if (StringUtils.isEmpty(user.getUsername()) || StringUtils.isEmpty(user.getPassword())) {
+            request.setAttribute("msg", "用户名或密码不能为空！");
+            return "login";
+        }
+        Subject subject = SecurityUtils.getSubject();
+        UsernamePasswordToken token=new UsernamePasswordToken(user.getUsername(),user.getPassword());
+        try {
+            subject.login(token);
+            return "index";
+        }catch (LockedAccountException lae) {
+            token.clear();
+            request.setAttribute("msg", "用户已经被锁定不能登录，请与管理员联系！");
+            return "login";
+        } catch (AuthenticationException e) {
+            token.clear();
+            request.setAttribute("msg", "用户或密码不正确！");
+            return "login";
+        }
+    }
+
+}
